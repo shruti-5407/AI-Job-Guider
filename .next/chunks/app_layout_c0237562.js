120a4a0dcfc7c761eca99f2a53f3eb8fe2ecab9e
Shruti Pandey <shrutipandey592@gmail.com>
2025-06-03 19:57:14 +0530

    Add files via upload

---
+++ .next/chunks/app_layout_c0237562.js
@@ -0,0 +1,15 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__a917f39f._.css",
+  "static/chunks/node_modules_@clerk_nextjs_dist_esm_app-router_7dd730b3._.js",
+  "static/chunks/node_modules_next_c4290ecc._.js",
+  "static/chunks/node_modules_@clerk_shared_dist_66faf0e2._.js",
+  "static/chunks/node_modules_swr_dist_d3931529._.js",
+  "static/chunks/node_modules_@clerk_clerk-react_dist_3d5ca8b9._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_d0a8f9d6._.js",
+  "static/chunks/_d63f5e1a._.js"
+],
+    source: "dynamic"
+});
---
+++ .next/chunks/app_page_jsx_d1edd8d9._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_8a51fa7a._.js"
+],
+    source: "dynamic"
+});
