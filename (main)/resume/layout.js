042091960310b7874852dda6a98798f46a607b9f
Shruti Pandey <shrutipandey592@gmail.com>
2025-06-03 20:02:04 +0530

    Add files via upload

---
+++ (main)/resume/layout.js
@@ -0,0 +1,31 @@
+
+
+
+import { Suspense } from "react";
+import { BarLoader } from "react-spinners";
+
+export default function Layout({ children }) {
+  return (
+    <div className="px-5">
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="gray" />}
+      >
+        {children}
+      </Suspense>
+    </div>
+  );
+}
+
+
+
+// app/resume/layout.js
+// export default function Layout({ children }) {
+//     return (
+//       <div className="px-5">
+//         <Suspense fallback={<BarLoader className="mt-4" width={"100%"} color="gray" />}>
+//           {children}
+//         </Suspense>
+//       </div>
+//     );
+//   }
+  
---
+++ api/inngest/route.js
@@ -0,0 +1,19 @@
+
+
+
+
+import { inngest } from "@/lib/inngest/client";
+import { generateIndustryInsights } from "@/lib/inngest/function";
+
+
+import {serve} from "inngest/next";
+
+export  const { GET,POST,PUT}=serve({
+    client:inngest,
+    functions:[
+        generateIndustryInsights,
+
+
+
+    ],
+});
